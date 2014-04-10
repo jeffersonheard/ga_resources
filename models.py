c70971d291474d6bfb29485970f0fd3fca75c30f
Jeff Heard <jefferson.r.heard@gmail.com>
2014-04-10 12:36:29 -0400

    Page permissions changed

--- models.py
+++ models.py
@@ -34,66 +34,13 @@ def get_user(request):
     else:
         return request.user
 
-class PagePermissionsMixin(object):
-    def storage_key(self, kfor):
-        return "{label}.{model}.{key}.{kfor}".format(
-            label=self._meta.app_label,
-            model=self._meta.module_name,
-            key=self.pk,
-            kfor=kfor
-        )
-
-    @property
-    def permissions_store(self):
-        return s.PERMISSIONS_DB
-
-    @property
-    def edit_users(self):
-        return set(int(x) for x in self.permissions_store.smembers(self.storage_key('edit-users')))
-
-    @property
-    def edit_groups(self):
-        return set(int(x) for x in self.permissions_store.smembers(self.storage_key('edit-groups')))
-
-    @property
-    def view_users(self):
-        return set(int(x) for x in self.permissions_store.smembers(self.storage_key('view-users')))
-
-    @property
-    def view_groups(self):
-        return set(int(x) for x in self.permissions_store.smembers(self.storage_key('view-groups')))
-
-
-    def add_edit_user(self, user):
-        self.permissions_store.sadd(self.storage_key('edit-users'), user if isinstance(user, int) else user.pk)
-
-
-    def add_view_user(self, user):
-        self.permissions_store.sadd(self.storage_key('view-users'), user if isinstance(user, int) else user.pk)
-
-
-    def add_edit_group(self, group):
-        self.permissions_store.sadd(self.storage_key('edit-groups'), group if isinstance(group, int) else group.pk)
-
-
-    def add_view_group(self, group):
-        self.permissions_store.sadd(self.storage_key('view-groups'), group if isinstance(group, int) else group.pk)
-
-
-    def remove_edit_user(self, user):
-        self.permissions_store.srem(self.storage_key('edit-users'), user if isinstance(user, int) else user.pk)
-
-
-    def remove_view_user(self, user):
-        self.permissions_store.srem(self.storage_key('view-users'), user if isinstance(user, int) else user.pk)
-
-
-    def remove_edit_group(self, group):
-        self.permissions_store.srem(self.storage_key('edit-groups'), group if isinstance(group, int) else group.pk)
-
-
-    def remove_view_group(self, group):
-        self.permissions_store.srem(self.storage_key('view-groups'), group if isinstance(group, int) else group.pk)
+class PagePermissionsMixin(models.Model):
+    owner = models.ForeignKey(User, related_name='owned_%(app_label)s_%(class)s', null=True)
+    publicly_viewable = models.BooleanField(default=True)
+    edit_users = models.ManyToManyField(User, related_name='editable_%(app_label)s_%(class)s', null=True, blank=True)
+    view_users = models.ManyToManyField(User, related_name='viewable_%(app_label)s_%(class)s', null=True, blank=True)
+    edit_groups = models.ManyToManyField(User, related_name='group_editable_%(app_label)s_%(class)s', null=True, blank=True)
+    view_groups = models.ManyToManyField(User, related_name='group_viewable_%(app_label)s_%(class)s', null=True, blank=True)
 
     def can_add(self, request):
         return self.can_change(request)
@@ -101,11 +48,8 @@ class PagePermissionsMixin(object):
     def can_delete(self, request):
         return self.can_change(request)
 
-
     def can_change(self, request):
         user = get_user(request)
-
-        ret = True
 
         if user.is_authenticated():
             if not self.owner:
@@ -113,13 +57,10 @@ class PagePermissionsMixin(object):
             elif user.pk == self.owner.pk:
                 ret = True
             else:
-                users = self.edit_users
-                groups = self.edit_groups
-
-                if len(users) > 0 and user.pk in users:
+                if self.edit_users.filter(pk=user.pk).exists():
                     ret = True
-                elif len(groups) > 0:
-                    ret = user.groups.filter(pk__in=groups).exists()
+                if self.edit_groups.filter(pk__in=[g.pk for g in user.groups.all()]):
+                    ret = True
                 else:
                     ret =  False
         else:
@@ -127,82 +68,52 @@ class PagePermissionsMixin(object):
 
         return ret
 
-
     def can_view(self, request):
         user = get_user(request)
+
         if self.public or not self.owner:
-            return True
+            ret = True
+
         if user.is_authenticated():
             if not self.owner:
-                return user.is_superuser
+                ret = user.is_superuser
             elif user.pk == self.owner.pk:
-                return True
+                ret = True
             else:
-                users = self.view_users
-                groups = self.view_groups
-
-                if len(users) > 0 and user.pk in users:
-                    return True
-                elif len(groups) > 0:
-                    return user.groups.filter(pk__in=groups).exists()
+                if self.view_users.filter(pk=user.pk).exists():
+                    ret = True
+                if self.view_groups.filter(pk__in=[g.pk for g in user.groups.all()]):
+                    ret = True
                 else:
-                    return False
+                    ret = False
         else:
-            return False
+            ret = False
 
-    def copy_permissions_to_children(self, clear_existing=True, recurse=False):
+        return ret
+
+    def copy_permissions_to_children(self, recurse=False):
         # pedantically implemented.  should use set logic to minimize changes, but ptobably not important
         for child in self.children:
             if isinstance(child, PagePermissionsMixin):
-                if clear_existing:
-                    for u in (int(x) for x in child.edit_users):
-                        child.remove_edit_user(u)
-                    for u in (int(x) for x in child.view_users):
-                        child.remove_view_user(u)
-                    for g in (int(x) for x in child.edit_groups):
-                        child.remove_edit_group(g)
-                    for g in (int(x) for x in child.view_groups):
-                        child.remove_view_group(g)
-
-                for u in (int(x) for x in self.edit_users):
-                    child.add_edit_user(u)
-                for u in (int(x) for x in self.view_users):
-                    child.add_view_user(u)
-                for g in (int(x) for x in self.edit_groups):
-                    child.add_edit_group(g)
-                for g in (int(x) for x in self.view_groups):
-                    child.add_view_group(g)
-
+                child.edit_users = [u for u in self.edit_users.all()]
+                child.view_users = [u for u in self.view_users.all()]
+                child.edit_groups = [g for g in self.edit_groups.all()]
+                child.view_groups = [g for g in self.view_groups.all()]
                 child.public = self.public
                 child.owner = self.owner
                 child.save()
                     
                 if recurse:
-                    child.copy_permissions_to_children(clear_existing=clear_existing, recurse=recurse)
+                    child.copy_permissions_to_children(recurse=True)
 
 
-    def copy_permissions_from_parent(self, clear_existing=True):
+    def copy_permissions_from_parent(self):
         parent = self.parent.get_content_model()
         if isinstance(parent, PagePermissionsMixin):
-            if clear_existing:
-                for u in (int(x) for x in self.edit_users):
-                    self.remove_edit_user(u)
-                for u in (int(x) for x in self.view_users):
-                    self.remove_view_user(u)
-                for g in (int(x) for x in self.edit_groups):
-                    self.remove_edit_group(g)
-                for g in (int(x) for x in self.view_groups):
-                    self.remove_view_group(g)
-
-            for u in (int(x) for x in self.parent.edit_users):
-                self.add_edit_user(u)
-            for u in (int(x) for x in self.parent.view_users):
-                self.add_view_user(u)
-            for g in (int(x) for x in self.parent.edit_groups):
-                self.add_edit_group(g)
-            for g in (int(x) for x in self.parent.view_groups):
-                self.add_view_group(g)
-
+            self.view_groups = [g for g in self.parent.view_groups.all()]
+            self.edit_groups = [g for g in self.parent.edit_groups.all()]
+            self.view_users = [u for u in self.parent.view_users.all()]
+            self.edit_users = [u for u in self.parent.edit_users.all()]
             self.public = self.parent.public
             self.owner = self.parent.owner
             self.save()
@@ -212,10 +123,10 @@ class PagePermissionsMixin(object):
 # page_processor_for(MyPage)(ga_resources.views.page_permissions_page_processor)
 def page_permissions_page_processor(request, page):
     page = page.get_content_model()
-    edit_groups = Group.objects.filter(pk__in=page.edit_groups)
-    view_groups = Group.objects.filter(pk__in=page.view_groups)
-    edit_users = User.objects.filter(pk__in=page.edit_users)
-    view_users = User.objects.filter(pk__in=page.view_users)
+    edit_groups = page.edit_groups.all()
+    view_groups = page.view_groups.all()
+    edit_users = page.edit_users.all()
+    view_users = page.view_users.all()
 
     return {
         "edit_groups": edit_groups,
